463729f9cd4c6382a0352ab348f4e3fc9b5663b4
MSI\steve <stevelynagh@hotmail.com>
2019-07-24 23:38:11 +0200

    Updated profile text and image

--- includes/home.php
+++ includes/home.php
@@ -42,19 +42,19 @@
 	</section><!--/.carousel-->
 		
 		
-		<!--Begin Welcome Info-->
+		<!--Welcome-->
 		<section class="panel panel-default">
 		<div class="welcome panel-body justify">
-			<p><img src="img/portrait.jpg" onload="imgLoaded(this)">Greetings! I’m Steven Joseph Lynagh. I’m the creative type. I can draw. I can arrange shapes on a page. I can make those shapes move. I can code too. I can even play the drums. I have more than 10 years experience in the creative industries as an animator, illustrator, and designer.  I’ve just started making web sites and mobile apps and want to make more. You might call me a multidisciplinarian. Maybe a polymath. A renaissance man. Whatever, just calm down.</p>
+			<p><img height="128" width="128" src="img/desert_steve_256.jpg" onload="imgLoaded(this)">I’m Steven Joseph Lynagh, a Frontend Developer with a background in design and multimedia. My weapons of choice are Javascript (ES6 of course!), CSS, and HTML. And yeah I could tell you I’m an expert when it comes to jQuery, Bootstrap, or Photoshop  but I’d rather <a href="https://app.pluralsight.com/profile/steven-lynagh" target="_blank">show you the proof</a>. Maybe you need a React developer? I've built my <a href="http://resume.stevenlynagh.com/" target="_blank" >resume as a React app</a>.</p>
+
+			<p>I am versatile and resourceful – traits from a career which spans animation, illustration and  multimedia to e-learning and frontend design and development for web and mobile. Maybe I’m too eager to pick up a new tool to apply to a solution, but I reckon that’s a strength. You might call me a polymath. Maybe a renaissance man. Whatever, just calm down.</p>
 		
-			<p>When I’m not banging my head against a light box or a computer screen you might find me banging the drums for Brisbane cult rock legends <a href="https://www.facebook.com/pages/Giants-of-Science/78827037264?fref=ts" target="_blank"><i>Giants of Science</i></a>. I like quantum physics, coffee and cricket. I think Norgwegian “Black and Roll” band <em>Kvelertak</em> are cool. I was once a chalet host in the French Alps.</p>
+			<p>When I’m not banging my head against a computer screen you might find me banging the drums for Brisbane cult rock legends <a href="https://www.facebook.com/pages/Giants-of-Science/78827037264?fref=ts" target="_blank"><i>Giants of Science</i></a>. I like quantum physics, coffee and cricket. I think Norwegian “Black and Roll” band <em>Kvelertak</em> are cool.</p>
 		
-			<p>I’m driven by a passion for quality. I believe in doing things right, whether it’s getting a design pixel-perfect or making sure a client is 100% satisfied. While your here, feel free to check out my <a href="index.php?page=portfolio">portfolio</a>. If you think we could work together, why don't you <a href="index.php?page=contact">drop me a line</a>?</p>
+			<p>I’m driven by a passion for quality and believe in doing things right. While your here, feel free to check out my <a href="index.php?page=portfolio">portfolio</a>. If you think we could work together, why don't you <a href="index.php?page=contact">drop me a line</a>?</p>
 			
 			</div><!--/.welcome-->
-		</section>
-		
-		<!--Begin Timeline-->
+		</section>		
 	
 	
 	</div><!--container--> 

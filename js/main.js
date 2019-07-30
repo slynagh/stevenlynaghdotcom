eadc2e43d5356453259dee9522bee6fceaaa6387
MSI\steve <stevelynagh@hotmail.com>
2019-07-30 21:50:45 +0200

    refactor thumb animations to use css, add small delay to trigger (less annoying)

--- js/main.js
+++ js/main.js
@@ -14,74 +14,74 @@ function hideNavLabels(){
 		navLiWidthSmall = a+b;
 	
 		//select all .nav lis without class of .active 
-		$('.nav li:not(.active)')
-		.each(function(index) {
+		$('.nav li:not(.active)').each(
+			function(index) {			
+				$this = $(this);
 				
-			$this = $(this);
-			
-			//Push width to navLiWidthLarge[]
-			myWidth = $this.outerWidth(false);
-			navLiWidthLarge.push(myWidth);
-			
-			//and set width to navLiWidthSmall
-			$this.css('width',navLiWidthSmall + 'px');
-			//set navlabel to hidden so IE doesn't chuck a spaz
-			$this.find('.navlabel').addClass('hidden');
-			
-			//add hover functionality
-			$this.hover(
-				function(e){
-					//Mouseover: set width to large
-					lgWidth=navLiWidthLarge[index];
-					$(this).stop().animate({width:lgWidth +1+'px'}, 200, function(){
-						$(this).find('.navlabel').removeClass('hidden')
-					})
-				},function(e){
-					//MouseOut: return width to Small
-					$(this).find('.navlabel').addClass('hidden');
-					$(this).stop().animate({width:navLiWidthSmall +'px'}, 200);
-				}
-			);
-		})
-			
-	//finally, show the nav			
-	fadeInNav();
+				//Push width to navLiWidthLarge[]
+				myWidth = $this.outerWidth(false);
+				navLiWidthLarge.push(myWidth);
+				
+				//and set width to navLiWidthSmall
+				$this.css('width',navLiWidthSmall + 'px');
+				//set navlabel to hidden so IE doesn't chuck a spaz
+				$this.find('.navlabel').addClass('hidden');
+				
+				//add hover functionality
+				$this.hover(
+					function(e){
+						//Mouseover: set width to large
+						lgWidth=navLiWidthLarge[index];
+						$(this).stop().animate({width:lgWidth +1+'px'}, 200, function(){
+							$(this).find('.navlabel').removeClass('hidden')
+						})
+					},function(e){
+						//MouseOut: return width to Small
+						$(this).find('.navlabel').addClass('hidden');
+						$(this).stop().animate({width:navLiWidthSmall +'px'}, 200);
+					}
+				);
+			}
+		);
+					
+	fadeInNav(); //show the nav	
 	
-}// makeNavCool()
-
+}
 function initThumbAnimation(){
+	var activeClass="active"; 
+
 	$('.tn').hover(function(e){
-		//handler in
-		//$(this).children('img').stop().animate({top:"220px"}, 400, 'easeInQuart');
-		$(this).children('img').hoverFlow(e.type, {top:"220px"}, 400, 'easeInQuad');
-	},function(e){
-		//handler out
-		//$(this).children('img').stop().animate({top:"0px"}, 400, 'easeOutQuart');
-		$(this).children('img').hoverFlow(e.type, {top:"0px"}, 200, 'easeOutQuad');
-	});
+		//var active = false;
+		var $this = $(e.currentTarget);
+		var timer = setTimeout(function(){
+			
+			if ($this.is(':hover')){
+				$this.addClass(activeClass);
+			}
+		}, 100);
 		
+	},function(e){
+		$(e.currentTarget).removeClass(activeClass);
+	});
+}
+
+function handleScroll(){
+	//remove 'full' class from navbar on scroll and replace when at top of document
+	//$(window).scroll(function () {
+	//		if($(document).scrollTop()>0)
+	//			{
+	//				$('.navbar').removeClass('full', 400, 'easeInCubic')
+	//			}
+	//		else if($(document).scrollTop()===0)
+	//			{
+	//				$('.navbar').addClass('full', 400, 'easeOutCubic');
+	//			}		
+	//	});
 }
 
 $(document).ready(function(){
 	initThumbAnimation();
-	
-	
-	
-	//remove 'full' class from navbar on scroll and replace when at top of document
-	//$(window).scroll(function () {
-//		if($(document).scrollTop()>0)
-//			{
-//				$('.navbar').removeClass('full', 400, 'easeInCubic')
-//			}
-//		else if($(document).scrollTop()===0)
-//			{
-//				$('.navbar').addClass('full', 400, 'easeOutCubic');
-//			}		
-//	});//window scroll
-
-
-	
-});//function
+});
 
 $(window).load(function() {
 	if(!Modernizr.touch){//we want to keep the labels on touch screens
@@ -89,6 +89,4 @@ $(window).load(function() {
 	} else {
 		fadeInNav();
 	}
-	
-
 });
